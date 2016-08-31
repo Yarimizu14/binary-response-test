c450bf18805ed7f839b09da2dd47dc3caaf2e799
Kensei Yarimizu <yarimizu.kensei14@gmail.com>
2016-09-01 01:10:46 +0900

    temporary impl

--- app/controllers/capture_controller.rb
+++ app/controllers/capture_controller.rb
@@ -1,2 +1,21 @@
+require 'base64'
+
 class CaptureController < ApplicationController
+
+  def screen
+    @base64 = Base64.encode64(binary)
+  end
+
+  def connect
+    send_data binary, type: 'image/jpeg', disposition: 'inline'
+  end
+
+  private
+
+  def binary
+    path = "#{Rails.root}/tmp/test.jpg"
+    img = File.open(path, 'r')
+    img.read
+  end
+
 end
